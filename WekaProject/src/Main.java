b0adc4fc0dd592fe57931a63eeb666a205dba0aa
Exypte <thib.condemine@outlook.com>
2019-03-19 17:45:44 +0100

    Ajout de la crossValidation

--- WekaProject/src/Main.java
+++ WekaProject/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -13,8 +16,13 @@ public class Main {
 			instances.setClassIndex(instances.numAttributes() - 1);
 			System.out.println(instances);
 			J48 j48 = new J48();
+			j48.setMinNumObj(5);
 			j48.buildClassifier(instances);
 			System.out.println(j48.graph());
+			 Evaluation evaluation = new Evaluation(instances);
+			 //evaluation.evaluateModel(j48, instances);
+			 evaluation.crossValidateModel(j48, instances, 5, new Random(1));
+			 System.out.println(evaluation.toSummaryString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
