083b16221a7ec2140396ec0e8cfeb807ddec393b
Exypte <thib.condemine@outlook.com>
2019-03-19 17:07:56 +0100

    Creation de l'arbre

--- WekaProject/src/Main.java
+++ WekaProject/src/Main.java
@@ -1,9 +1,24 @@
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		try {
+			DataSource source = new DataSource("/home/etudiant/weka-3-8-3/data/iris.arff");
+			Instances instances = source.getDataSet();
+			instances.setClassIndex(instances.numAttributes() - 1);
+			System.out.println(instances);
+			J48 j48 = new J48();
+			j48.buildClassifier(instances);
+			System.out.println(j48.graph());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
