44b59355c3e51ca0cc2fdd416a209ca01f616f79
Exypte <thib.condemine@outlook.com>
2019-03-27 09:22:09 +0100

    Save

--- WekaProject/src/Main.java
+++ WekaProject/src/Main.java
@@ -8,40 +8,36 @@ import java.util.Random;
 import com.opencsv.CSVWriter;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.evaluation.output.prediction.CSV;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
-import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.gui.explorer.VisualizePanel;
-import weka.gui.visualize.AttributePanel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	    // first create file object for file placed at location 
-	    // specified by filepath 
-	    File file = new File("test.csv");
-	    
-        // create FileWriter object with file as parameter 
-        FileWriter outputfile = null;
+		// first create file object for file placed at location 
+		// specified by filepath 
+		File file = new File("test.csv");
+
+		// create FileWriter object with file as parameter 
+		FileWriter outputfile = null;
 		try {
 			outputfile = new FileWriter(file);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}; 
-  
-        // create CSVWriter object filewriter object as parameter 
-        CSVWriter writer = new CSVWriter(outputfile);
-        
-        // create a List which contains String array 
-        List<String[]> header = new ArrayList<String[]>(); 
-        header.add(new String[] { "Nombre d'exemples min par feuille", "% de bien classé" });
+
+		// create CSVWriter object filewriter object as parameter 
+		CSVWriter writer = new CSVWriter(outputfile);
+
+		// create a List which contains String array 
+		List<String[]> header = new ArrayList<String[]>(); 
+		header.add(new String[] { "Nombre d'exemples min par feuille", "% de bien classé" });
 		writer.writeAll(header);
-		
+
 		try {
 			DataSource source = new DataSource("/home/etudiant/weka-3-8-3/data/iris.arff");
 
@@ -53,23 +49,23 @@ public class Main {
 			J48 j48 = new J48();
 
 			for(int i = 2; i < instances.numInstances()*0.1; i++) {
-				
-		        List<String[]> data = new ArrayList<String[]>(); 
-		        
+
+				List<String[]> data = new ArrayList<String[]>(); 
+
 				j48.setMinNumObj(i);
 				j48.buildClassifier(instances);
-				
+
 				Evaluation evaluation = new Evaluation(instances);
 				evaluation.crossValidateModel(j48, instances, 5, new Random(1));
-				
+
 				System.out.println(evaluation.toSummaryString());
-			  
-			    data.add(new String[] {Integer.toString(i), Double.toString(evaluation.pctCorrect())}); 
+
+				data.add(new String[] {Integer.toString(i), Double.toString(evaluation.pctCorrect())}); 
 				writer.writeAll(data);
 			}
 
-	        // closing writer connection 
-	        writer.close(); 
+			// closing writer connection 
+			writer.close(); 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
