c7b14d9a0e56d5d7eab21608f936336d7144eb93
Exypte <thib.condemine@outlook.com>
2019-04-08 10:08:19 +0200

    final

--- WekaProject/src/Main.java
+++ WekaProject/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Algo algo = new Algo("data/iris.arff");
+
+		Algo algo = new Algo("iris.arff");
 		
 		InterfaceControler controler = new ControlerCommands(algo);
 		
